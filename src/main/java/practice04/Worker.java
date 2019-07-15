6763b1555d02882de14466a1179c5d79d5e6e138
Songbai Yan <colorguitar@hotmail.com>
2019-07-15 17:31:44 +0800

    finish 4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,12 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is Tom. I am 21 years old. I am a Worker. I have a job.";
+    }
 }
