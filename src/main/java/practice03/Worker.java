11760a73faaa6b81f6b87bb2b138ad38abd98e42
Songbai Yan <colorguitar@hotmail.com>
2019-07-15 17:04:26 +0800

    finish 3

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,31 @@
 package practice03;
 
 public class Worker {
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
