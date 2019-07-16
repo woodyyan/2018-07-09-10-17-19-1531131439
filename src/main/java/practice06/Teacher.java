762fdc6ab9b0c71885f3fd70e86b793bf7fe9baf
Songbai Yan <colorguitar@hotmail.com>
2019-07-16 10:06:44 +0800

    finish 6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,30 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == 0) {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.";
+    }
 }
