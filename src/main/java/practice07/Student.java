114f70e7392fe9665d82fd2414ca125e54017f78
Songbai Yan <colorguitar@hotmail.com>
2019-07-16 10:21:21 +0800

    finish 7

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,19 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private final Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.";
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass == null) {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+        return "My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.";
+    }
 }
